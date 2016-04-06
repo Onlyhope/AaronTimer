4932929ec459a7c584fcbb0e61dbcad516a03824
Onlyhope <leeaaron326@gmail.com>
2016-04-06 02:45:10 -0400

    Created quit button

--- src/controllers/TimerController.java
+++ src/controllers/TimerController.java
@@ -41,6 +41,10 @@ public class TimerController {
         }
     }
     
+    public void handleQuitTimer() {
+        System.exit(0);
+    }
+    
     public void handleSaveTimer() {
        timerFileManager.saveTimers();
     }
